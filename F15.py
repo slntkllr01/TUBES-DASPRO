37ac823703eba8408be02f8cae5973ccc2f95520
slntkllr01 <raffaelsiahaan@gmail.com>
2023-04-14 22:59:52 +0700

    Modify File

--- F15.py
+++ F15.py
@@ -1,14 +1,75 @@
 def help():
   global loginstatus, role
   if loginstatus == False:
-    print('=========== HELP ===========')
+     print(f"""
+=========== HELP ===========
+1. login
+   Untuk masuk menggunakan akun
+2. load
+   Untuk memuat file eksternal ke dalam permainan
+3. save
+   Untuk menyimpan data permainan
+4. exit
+   Untuk keluar dari permainan""")
   else:
       if role == 'bandung_bondowoso' :
-        print('=========== HELP ===========')
+        print(f"""
+=========== HELP ===========
+1. logout
+   Untuk keluar dari akun yang digunakan sekarang
+2. summonjin
+   Untuk memanggil jin
+3. hapusjin
+   Untuk menghilangkan jin
+4. ubahjin
+   Untuk mengubah tipe jin
+5. batchkumpul
+   Untuk memerintahkan seluruh jin pengumpul mengumpulkan bahan
+6. batchbangun
+   Untuk memerintahkan seluruh jin pembangun membangun candi
+7. laporanjin
+   Untuk mengambil laporan kinerja seluruh jin
+8. laporancandi
+   Untuk mengambil laporan progress pembangunan candi
+9. save
+   Untuk menyimpan data permainan
+10. exit
+   Untuk keluar dari permainan""")
       elif role == 'roro_jonggrang':
-        print('=========== HELP ===========')
+        print(f"""
+=========== HELP ===========
+1. logout
+   Untuk keluar dari akun yang digunakan sekarang
+2. hancurkancandi
+   Untuk menghancurkan candi yang tersedia
+3. ayamberkokok
+   Untuk menyelesaikan permainan
+4. save
+   Untuk menyimpan data permainan
+5. exit
+   Untuk keluar dari permainan""")
       else : #Jin
         if role == 'pengumpul':
-          print('=========== HELP ===========')
+          print(f"""
+=========== HELP ===========
+1. logout
+   Untuk keluar dari akun yang digunakan sekarang
+2. kumpul
+   Untuk mengumpulkan resource candi
+3. save
+   Untuk menyimpan data permainan
+4. exit
+   Untuk keluar dari permainan""")
         else : # role = Pembangun
-          print('=========== HELP ===========')
+          print(f"""
+=========== HELP ===========
+1. logout
+   Untuk keluar dari akun yang digunakan sekarang
+2. bangun
+   Untuk membangun candi
+3. save
+   Untuk menyimpan data permainan
+4. exit
+   Untuk keluar dari permainan""")
+          
+# Note : Pengumpul dan Pembangun tidak bisa exit          
