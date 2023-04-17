9be8b4881c47c560bdf7c192ba8a897b420549a1
Nurbaedahawaliyah <nurbaedahawaliyah22@gmail.com>
2023-04-17 21:35:45 +0700

    udah fix

--- F06.py
+++ F06.py
@@ -1,1 +1,8 @@
-def bangun():
+import random
+def summaterials() :
+    pasir = random.randrange(0,5)
+    batu = random.randrange(0,5)
+    air = random.randrange(0,5)
+    output = ("Jin menemukan", pasir, "pasir",batu, "batu",air,"air")
+    return output
+print (summaterials())
