3bc1fea7887aa26770c7369e28e23b406484d642
reiman21 <radinkarei@gmail.com>
2023-04-28 23:11:34 +0700

    adding F09 and F10 file

--- F09.py
+++ F09.py
@@ -1,1 +1,70 @@
-def laporanjin():
+from F01 import *
+from database import *
+from csvfunction import *
+from function import *
+
+print(">>>laporan jin")
+def laporanjin() :
+    if login() == True :
+        #Menghitung jin kumpul, jin bangun, dan total jin
+        for i in range(1,array_length(user)) :
+            jin_kumpul = 0
+            jin_bangun = 0
+
+            if user[i][2] == "Pengumpul" :
+                jin_kumpul += 1
+            elif user[i][2] == "Pembangun":
+                jin_bangun += 1
+        total_jin = jin_bangun + jin_kumpul
+
+
+        #Mendapatkan Jin Terajin dan Jin Termalas
+        if (array_length(candi) - array_kosong_count(candi)) - 1 > 0 :
+            max_candi = -1 
+            min_candi = 9999
+            for i in range(1,array_length(user)) : 
+                for j in range(1,array_length(candi)) : 
+                    count = 0
+                    if candi[j][1] == user[i][0] : 
+                        count += 1
+                    
+                    # Mendapatkan jin terajin
+                    if count > max_candi :
+                        max_candi = count 
+                        terajin = user[i][0]
+                    elif count  == max_candi : 
+                            if user[i][0] < terajin : 
+                                terajin = user[i][0]
+                                max_candi = count 
+                    
+                    # Mendapatkan jin termalas
+                    if count < min_candi :
+                        min_candi = count 
+                        termalas = user[i][0]
+                    elif count  == min_candi : 
+                            if user[i][0] > termalas and user[i][2] == "Pembangun" : 
+                                termalas = user[i][0]
+                                min_candi = count 
+
+            else : 
+                terajin = termalas = "-"
+                 
+
+        #Mendapatkan jumlah pasir, jumlah air, jumlah batu yang tersedia
+        jumlah_batu = bahan_bangunan[1][2]
+        jumlah_pasir = bahan_bangunan[2][2]
+        jumlah_air = bahan_bangunan[3][2]
+
+        print(">>>laporan jin")
+        print(f"Total Jin : {total_jin} ")
+        print(f"Total Jin Pengumpul : {jin_kumpul} ")
+        print(f"Total Jin Pembangun : {jin_bangun} ")
+        print(f"Jin terajin : {terajin}")
+        print(f"Jin termalas : {termalas}")
+        print(f"Jumlah Batu :{jumlah_batu}")
+        print(f"Jumlah Pasir :{jumlah_pasir}")
+        print(f"Jumlah air :{jumlah_air} ")
+        
+    else :
+        print("Laporan ini hanya bisa diakses oleh akun Bondowoso")
+
--- F10.py
+++ F10.py
@@ -1,1 +1,44 @@
-def laporancandi()
+from F01 import *
+from database import *
+from csvfunction import *
+from function import *
+
+def harga_candi(candi, idx_candi) : 
+    return  1000*candi[idx_candi][2] + 15000*candi[idx_candi][3]  + 7500*candi[idx_candi][4] 
+
+def laporancandi() :
+    harga_termurah = 9999999999 
+    harga_termahal = 0
+    id_termurah = id_termahal = 0 
+
+    if login() == True :
+        jumlah_pasir = 0
+        jumlah_batu = 0 
+        jumlah_air = 0
+        total_candi = (array_length(candi) - array_kosong_count(candi)) - 1
+
+        if total_candi != 0 : 
+            for i in range(1, array_length(candi) + 1) : 
+                jumlah_pasir += candi[i][2]
+                jumlah_batu += candi[i][3] 
+                jumlah_air += candi[i][4]
+
+                harga_candi = harga_candi(candi,i)
+                if harga_candi > harga_termahal : 
+                    harga_termahal = harga_candi
+                    id_termahal = i
+
+
+                if harga_candi < harga_termurah : 
+                    harga_termurah = harga_candi
+                    id_termurah = i
+        
+        else :
+            id_termurah = id_termahal = "-"
+
+        print(f"> Total Candi: {total_candi}")
+        print(f"> Total Pasir yang digunakan: {jumlah_pasir}")
+        print(f"> Total Batu yang digunakan: {jumlah_batu}")
+        print(f"> Total Air yang digunakan: {jumlah_air}")
+        print("> ID Candi Termahal:" + str(id_termahal) + "Rp{:,.2f}".format(id_termahal))
+        print("> ID Candi Termurah:" + str(id_termurah) + "Rp{:,.2f}".format(id_termurah))
